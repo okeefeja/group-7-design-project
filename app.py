ecc91414133448e1cd9a420d3f89e1fc7e1b45d9
AranQuintana <160046651+AranQuintana@users.noreply.github.com>
2024-02-15 02:34:06 +0000

    Flask, unsuccessful database integration

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os.path
+
+# this variable, db, will be used for all SQLAlchemy commands
+db = SQLAlchemy()
+# create the app
+app = Flask(__name__)
+# change string to the name of your database; add path if necessary
+db_name = 'fitness_app.db'
+# note - path is necessary for a SQLite db!!!
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, db_name)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+# initialize the app with Flask-SQLAlchemy
+db.init_app(app)
+
+
+# each table in the database needs a class to be created for it
+# this class is named Sock because the database contains info about socks
+# and the table in the database is named: socks
+# db.Model is required - don't change it
+# identify all columns by name and their data type
+class Muscles(db.Model):
+    __tablename__ = 'muscles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    body_part_id = db.Column(db.Integer)
+
+@app.route('/')
+def index():
+    try:
+        muscles = db.session.execute(db.select(Muscles)
+            .order_by(Muscles.name)).scalars()
+        muscle_text = '<ul>'
+        for muscle in muscles:
+            muscle_text += '<li>' + muscle.name
+        muscle_text += '</ul>'
+        return muscle_text
+    except Exception as e:
+        # e holds description of the error
+        error_text = "<p>The error:<br>" + str(e) + "</p>"
+        hed = '<h1>Something is broken.</h1>'
+        return hed + error_text
+
+if __name__ == '__main__':
+    app.run(debug=True)
