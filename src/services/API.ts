0e34c9b21e79f246460476518f06fb6cdca68c1f
Patrik Larsson <patrik@dictodesigns.com>
2024-02-23 12:19:20 +0000

    added workoutList types and fetch functionality

--- src/services/API.ts
+++ src/services/API.ts
@@ -1,12 +1,12 @@
-import { DummyList } from "../types/API";
+import { WorkoutProgramList } from "../types/API";
 
 const baseURL = "http://127.0.0.1:5000";
 
-async function fetchFromBackend(): Promise<DummyList | null> {
+async function fetchFromBackend(): Promise<WorkoutProgramList | null> {
   try {
     const response = await fetch(`${baseURL}/dev`);
     if (response.ok) {
-      const data: DummyList = await response.json();
+      const data: WorkoutProgramList = await response.json();
       return data;
     } else {
       return null;
