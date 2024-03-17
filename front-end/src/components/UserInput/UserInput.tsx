e38117e687d79f06cd049563caaee7dfaed8fe90
Patrik Larsson <patrik@dictodesigns.com>
2024-03-17 10:17:05 +0000

    further work on UI

--- front-end/src/components/UserInput/UserInput.tsx
+++ front-end/src/components/UserInput/UserInput.tsx
@@ -1,22 +1,34 @@
 import React, { useState } from "react";
 import { Text, View } from "react-native";
 import { ScTextInput } from "./UserInput.styled";
+import Spacer from "../Spacer/Spacer";
 
 interface UserInputProps {
+  title: string;
   placeholder: string;
+  textArea?: boolean;
 }
 
-export default function UserInput({ placeholder }: UserInputProps) {
+export default function UserInput({
+  title,
+  placeholder,
+  textArea = false,
+}: UserInputProps) {
   const [focused, setFocused] = useState(false);
 
   return (
     <View>
-      <Text style={{ color: "white" }}>Name</Text>
+      <Text style={{ color: "white", fontSize: 18, fontWeight: "600" }}>
+        {title}
+      </Text>
+      <Spacer orientation="vertical" size={2} />
       <ScTextInput
         onFocus={() => setFocused(true)}
         onBlur={() => setFocused(false)}
         placeholder={placeholder}
         focus={focused}
+        textArea={textArea}
+        multiline={textArea}
       />
     </View>
   );
--- front-end/src/screens/AddWorkoutProgramScreen/AddWorkoutProgramScreen.tsx
+++ front-end/src/screens/AddWorkoutProgramScreen/AddWorkoutProgramScreen.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 import { ScBaseContainer } from "../../components/BaseContainer/BaseContainer.styled";
-import Descriptor from "../../components/Descriptor/Descriptor";
 import { Text } from "react-native";
+import Descriptor from "../../components/Descriptor/Descriptor";
 import UserInput from "../../components/UserInput/UserInput";
+import Spacer from "../../components/Spacer/Spacer";
 
 export default function AddWorkoutProgramScreen() {
   return (
     <ScBaseContainer>
       <Descriptor title="Add workout program" />
-      <UserInput placeholder="Name" />
+      <Spacer orientation="vertical" size={4} />
+      <UserInput title="Name" placeholder="Name" />
+      <Spacer orientation="vertical" size={4} />
+      <UserInput
+        title="Description"
+        placeholder="Description"
+        textArea={true}
+      />
+      <Spacer orientation="vertical" size={4} />
+      <Text style={{ color: "white", fontSize: 18, fontWeight: "600" }}>
+        Add exercises
+      </Text>
+      <Text style={{ color: "gray", fontSize: 14, fontWeight: "400" }}>
+        Click on exercises to select!
+      </Text>
+      <Spacer orientation="vertical" size={4} />
     </ScBaseContainer>
   );
 }
